6aaf0d079fa5d0e5841b3a03346118f20215121b
root <root@master.local>
2021-07-05 14:10:16 +0300

    yaml dosyaalrı güncellendi

--- PythonApp/app.py
+++ PythonApp/app.py
@@ -1,20 +1,22 @@
 from typing import List, Dict
 from flask import Flask
+from flask import jsonify
 import mysql.connector
 import json
+import os
 
 app = Flask(__name__)
 
 
 def buyuk_sehirler() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'admin123',
-        'host': 'mydb',
-        'port': '3306',
-        'database': 'sehirler'
-    }
-    connection = mysql.connector.connect(**config)
+        
+    connection = mysql.connector.connect(
+        user = os.getenv('MYSQL_USERNAME'),
+        password = os.getenv('MYSQL_ROOT_PASSWORD'),
+        host = os.getenv('MYSQL_PORT_3306_TCP_ADDR'),
+        port = os.getenv('MYSQL_PORT_3306_TCP_PORT'),
+        database = os.getenv('MYSQL_INSTANCE_NAME')       
+    )    
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM buyuk_sehirler')
     results = cursor.fetchall()
@@ -23,11 +25,22 @@ def buyuk_sehirler() -> List[Dict]:
 
     return results
 
-
+healtcheck = {
+    "health": [
+    {
+       "status":"{ok}",  
+    }
+  ]  
+}
 @app.route('/')
 def index() -> str:
     return json.dumps({'buyuk_sehirler': buyuk_sehirler()})
 
+@app.route('/healtcheck')
+def run2():
+    return jsonify(healtcheck)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001)
+    app.run2(host='0.0.0.0',port=5001)
